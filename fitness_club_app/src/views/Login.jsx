47cfb6d47bd9e6de7d0c50c6460dea856af612fa
TatianaSlaw <slawiecka.t@gmail.com>
2023-03-06 13:24:16 +0100

    Toast Component from PrimeReact added to display messages about loging and signing users account.

--- fitness_club_app/src/views/Login.jsx
+++ fitness_club_app/src/views/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
@@ -6,13 +6,36 @@ import 'primereact/resources/themes/saga-blue/theme.css';   // theme
 import 'primereact/resources/primereact.css';                       // core css
 import 'primeicons/primeicons.css';                                 // icons
 import 'primeflex/primeflex.css';
+import {Toast} from "primereact/toast";
+
 import supabase from '../services/supabase';
 
+
 function Login() {
+    const toast = useRef(null);
+    const showSuccess = (e, msg) => {
+        e.preventDefault();
+        toast.current.show({
+            severity:'success',
+            summary: 'Success',
+            detail: msg,
+            life: 3000,
+        });
+    };
+    const showError = (e, msg) => {
+        toast.current.show({
+            severity:'error',
+            summary: 'Error',
+            detail: 'Something went wrong',
+            life: 3000,
+        });
+    };
+
     const [club_number, setClubNumber] = useState('');
     const [password, setPassword] = useState('');
     return (
         <div className="login-container">
+            <Toast ref={toast} />
              <h2><i className="pi pi-sign-in"></i>Log in your account</h2>
             <form className="login-form">
                 <span className="p-input-icon-left">
@@ -23,7 +46,7 @@ function Login() {
                     <Password className="p-inputtext-lg" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} toggleMask />
                 </span>
                 <span>Forgot password?</span>
-                <Button className="btn-primary" label="LOG IN" type="submit"  />
+                <Button onClick={(e) => showSuccess(e,'Congratulations! You are logged in your account')} className="btn-primary" label="LOG IN" type="submit"  />
                 <span><a href="/signup">Haven’t account?</a></span>
             </form>
         </div>
--- fitness_club_app/src/views/Signup.jsx
+++ fitness_club_app/src/views/Signup.jsx
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+
 import 'primereact/resources/themes/saga-blue/theme.css';   // theme
 import 'primereact/resources/primereact.css';                       // core css
 import 'primeicons/primeicons.css';                                 // icons
 import 'primeflex/primeflex.css';
+
 import supabase from '../services/supabase';
 
-function Login() {
+function Signup() {
+    const toast = useRef(null);
+
+    const showSuccess = (e,msg) => {
+        e.preventDefault();
+        toast.current.show({
+            severity:'success',
+            summary: 'Success',
+            detail: msg,
+            life: 3000,
+        });
+    };
+    const showError = (e,msg) => {
+        toast.current.show({
+            severity:'error',
+            summary: 'Error',
+            detail: msg,
+            life: 3000,
+        });
+    };
+
     const [club_number, setClubNumber] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     return (
         <div className="login-container">
+            <Toast ref={toast} />
             <h2>Create your account</h2>
             <form className="login-form">
                 <span className="p-input-icon-left">
@@ -30,11 +54,11 @@ function Login() {
                 <span className="p-inputtext-lg">
                     <Password placeholder="Reenter password" value={password} onChange={(e) => setPassword(e.target.value)} toggleMask />
                 </span>
-                <Button className="btn-primary" label="SIGN UP" type="submit"  />
+                <Button onClick={(e) => showSuccess(e,'Congratulations! You have successfully sign up your account')} className="btn-primary" label="SIGN UP" type="submit"  />
                 <span><a href="/login">Already have account?</a></span>
             </form>
         </div>
     )
 }
 
-export default Login;
+export default Signup;
