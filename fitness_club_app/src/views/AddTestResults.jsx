d4d17ef9d6dea786a9c6fc96459331d8214d0ad3
TatianaSlaw <slawiecka.t@gmail.com>
2023-03-09 10:37:25 +0100

    The Trainer component has been changed - the button AddTestResults has returned instead of the selection club memeber number field. The component Addtest results implements the loading of data from superbase table Clients.

--- fitness_club_app/src/views/AddTestResults.jsx
+++ fitness_club_app/src/views/AddTestResults.jsx
@@ -1,138 +1,72 @@
-import React, { useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
-import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
-
-import supabase from '../services/supabase';
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+import supabase from "../services/supabase";
 
 function AddTestResults() {
-    const toast = useRef(null);
-    const navigate = useNavigate();
+    const [clubNumber, setClubNumber] = useState("");
+    const [users, setUsers] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
+    async function fetchUsers() {
+        const { data, error } = await supabase
+            .from('Clients')
+            .select("id, club_number, name, surname, date_bd, height")
+            .eq('club_number', clubNumber);
 
-    const showSuccess = (msg) => {
-        toast.current.show({
-            severity: 'success',
-            summary: 'Success',
-            detail: msg,
-            life: 3000,
-        });
-    };
-    const showError = (msg) => {
-        toast.current.show({
-            severity: 'error',
-            summary: 'Error',
-            detail: msg,
-            life: 3000,
-        });
-    };
-
-    const handleNewMember = async (event) => {
-        event.preventDefault();
-
-        const club_number = club_numberRef.current.value;
-        const name = nameRef.current.value;
-        const surname = surnameRef.current.value;
-        const phone = phoneRef.current.value;
-        const date_bd = date_bdRef.current.value;
-        const height = heightRef.current.value;
-
-        let { data, error } = await supabase.auth.signInWithPassword({
-            club_number: club_number,
-            name: name,
-            surname: surname,
-            phone: phone,
-            date_bd: date_bd,
-            height: height,
-            user_type: "client",
-        });
-
-        if (data.user) {
-            //localStorage.setItem('userData', JSON.stringify(data.user));
-            showSuccess('You have add new data');
-            navigate("/trainer");
-        }
+        console.log(data)
+        console.log(error)
 
         if (error) {
-            showError(error.message);
+            console.error(error);
+            setErrorMessage("Error occurred while fetching data");
+        } else {
+            setUsers(data);
         }
-    };
+    }
 
-    const club_numberRef = useRef(null);
-    const nameRef = useRef(null);
-    const surnameRef  = useRef(null);
-    const phoneRef  = useRef(null);
-    const date_bdRef  = useRef(null);
-    const heightRef  = useRef(null);
+    function handleClubNumberChange(event) {
+        setClubNumber(event.target.value);
+    }
+
+    function handleFetchClick() {
+        if (clubNumber.length === 4) {
+            fetchUsers();
+        } else {
+            setErrorMessage("Club number must be exactly 4 digits");
+        }
+    }
 
     return (
-        <div className="newmember-container">
-            <Toast ref={toast} />
-            <h2>Add new club member info</h2>
-            <form className="login-form" onSubmit={handleNewMember}>
-                <div>
-                    <span className="input_label">Club number</span>
-                    <span className="p-inputtext-lg">
-                    <InputText
-                        className="p-inputtext-lg "
-                        placeholder="Club number"
-                        ref={club_numberRef} />
-                </span>
-                </div>
-                <div>
-                    <span className="input_label">First name</span>
-                    <span className="p-inputtext-lg">
-                    <InputText
-                        className="p-inputtext-lg"
-                        placeholder="First name"
-                        ref={nameRef} />
-                </span>
-                </div>
-                <div>
-                    <span className="input_label">Last name</span>
-                    <span className="p-inputtext-lg">
-                    <InputText
-                        className="p-inputtext-lg"
-                        placeholder="Last name"
-                        ref={surnameRef} />
-                </span>
-                </div>
-                <div>
-                    <span className="input_label">Mobile phone</span>
-                    <span className="p-inputtext-lg">
-                    <InputText
-                        className="p-inputtext-lg"
-                        placeholder="Mobile phone"
-                        ref={phoneRef} />
-                </span>
-                </div>
-                <div>
-                    <span className="input_label">Date of birth</span>
-                    <span className="p-inputtext-lg">
-                    <InputText
-                        className="p-inputtext-lg"
-                        placeholder="Date of birth"
-                        ref={date_bdRef} />
-                </span>
-                </div>
-                <div>
-                    <span className="input_label">Height</span>
-                    <span className="p-inputtext-lg">
-                    <InputText
-                        className="p-inputtext-lg"
-                        placeholder="Height"
-                        ref={heightRef} />
-                </span>
-                </div>
-                <Button
-                    className="btn-primary"
-                    label="ADD TEST RESULTS"
-                    type="submit" />
-                <span className="text-center"><a href="/trainer">Return to main dashboard</a></span>
-            </form>
+        <div>
+            <h2>Member {clubNumber} info</h2>
+            <div>
+                <span>Enter club number:</span>
+                <InputText
+                    value={clubNumber}
+                    onChange={handleClubNumberChange}
+                    maxLength={4}
+                    style={{ marginLeft: "8px" }}
+                />
+                <Button label="Select club member" onClick={handleFetchClick} />
+            </div>
+            {errorMessage && (
+                <div style={{ color: "red", marginTop: "8px" }}>{errorMessage}</div>
+            )}
+            <div>
+                {users.map((user) => (
+                        <div className="member-info" key={user.id}>
+                            <div>First name: <span className="test_value">{user.name}</span></div>
+                            <div>Last name: <span className="test_value">{user.surname}</span></div>
+                            <div>Date of birth: <span className="test_value">{user.date_bd}</span></div>
+                            <div>Height: <span className="test_value">{user.height}</span></div>
+                        </div>
+                ))}
+            </div>
+            <span className="text-center"><a href="/trainer">Return to main dashboard</a></span>
         </div>
-    )
+    );
 }
 
 export default AddTestResults;
--- fitness_club_app/src/views/Trainer.jsx
+++ fitness_club_app/src/views/Trainer.jsx
@@ -1,13 +1,8 @@
-import {Outlet, useLocation, useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import React, {useEffect, useRef, useState} from "react";
 import { Button } from "primereact/button";
 import { Toast } from 'primereact/toast';
-import { useFormik } from 'formik';
-import { InputText } from "primereact/inputtext";
-import { classNames } from 'primereact/utils';
-
 import supabase from '../services/supabase';
-
 function Trainer() {
     const toast = useRef(null);
     const navigate = useNavigate();
@@ -30,50 +25,15 @@ function Trainer() {
         });
     };
 
-    const cities = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
-    ];
-
-    const formik = useFormik({
-        initialValues: {
-            value: ''
-        },
-        validate: (data) => {
-            let errors = {};
-
-            if (!data.value) {
-                errors.value = 'Name - Surname is required.';
-            }
-
-            return errors;
-        },
-        onSubmit: (data) => {
-            data && show(data);
-            formik.resetForm();
-        }
-    });
-
-    const isFormFieldInvalid = (name) => !!(formik.touched[name] && formik.errors[name]);
-
-    const getFormErrorMessage = (name) => {
-        return isFormFieldInvalid(name) ? <small className="p-error">{formik.errors[name]}</small> : <small className="p-error">&nbsp;</small>;
-    };
-
-
+    const handleAddTestResult = () => {
+        navigate('/addtest')
+    }
     const handleNewMember = () => {
         navigate('/addnew')
     }
 
     const handleClubInfo = () => {
         navigate('/openhours')
-    }
-
-    const handleAddTestResult = () => {
-        navigate('/addtest')
     }
 
     const handleLogout = async () => {
@@ -83,72 +43,51 @@ function Trainer() {
             showError(error.message);
             return;
         }
-
         localStorage.removeItem('userData');
-        navigate('/login')
+        navigate('/')
     }
-
     const [isUserLogged, setUserLogged] = useState(null);
-
     useEffect(() => {
         setUserLogged(!!localStorage.getItem('user.data'))
     },[])
-
     return (
-            <div className="main-container">
-                <Toast ref={toast} />
-                <h1>
-                    Fitness Club Dashboard
-                </h1>
-
-                    <form onSubmit={formik.handleSubmit}
-                          className="flex flex-column align-items-center gap-2">
-                        <span className="p-float-label">
-                            <Toast ref={toast} />
-                            <InputText
-                                id="value"
-                                name="value"
-                                keyfilter="int"
-                                value={formik.values.value}
-                                onChange={(e) => {
-                                    formik.setFieldValue('value', e.target.value);
-                                }}
-                                className={classNames({ 'p-invalid': isFormFieldInvalid('value') })}
-                            />
-                            <label htmlFor="input_value">Club Number</label>
-                        </span>
-                        {getFormErrorMessage('city')}
-                        <Button onClick={handleAddTestResult}
-                                className="btn-primary btn-edit"
-                                type="submit"
-                                label="EDIT CLUB MEMBER INFO" />
-                    </form>
-
-                <div className="p-input-icon-right">
-                    <Button onClick={handleNewMember}
-                            className="btn-primary"
-                            severity="info"
-                            outlined
-                            label="Add new club member" />
-                    <i className="pi pi-user" style={{ fontSize: '1.5rem', color: "#2699f7" }}></i>
-                </div>
-                <div className="p-input-icon-right">
-                    <Button onClick={handleClubInfo}
-                            className="btn-primary"
-                            severity="info"
-                            outlined
-                            label="Add info for the coming holidays" />
-                    <i className="pi pi-calendar-plus" style={{ fontSize: '1.5rem', color: "#2699f7" }}></i>
-                </div>
-                <div>
-                    <Button onClick={handleLogout}
-                            className="btn-primary"
-                            label="LOG OUT"
-                            type="submit"  />
-                </div>
-
+        <div className="main-container">
+            <Toast ref={toast} />
+            <h1>
+                Fitness Club Dashboard
+            </h1>
+            <div className="p-input-icon-right">
+                <Button onClick={handleAddTestResult}
+                        className="btn-primary"
+                        severity="info"
+                        outlined
+                        label="Add test results" />
+                <i className="pi pi-user-edit" style={{ fontSize: '1.5rem',  color: "#2699f7" }}></i>
             </div>
-            )
-}
+            <div className="p-input-icon-right">
+                <Button onClick={handleNewMember}
+                        className="btn-primary"
+                        severity="info"
+                        outlined
+                        label="Add new club member" />
+                <i className="pi pi-user" style={{ fontSize: '1.5rem', color: "#2699f7" }}></i>
+            </div>
+            <div className="p-input-icon-right">
+                        <Button onClick={handleClubInfo}
+                        className="btn-primary"
+                        severity="info"
+                        outlined
+                        label="Add info for the coming holidays" />
+                <i className="pi pi-calendar-plus" style={{ fontSize: '1.5rem', color: "#2699f7" }}></i>
+            </div>
+            <div>
+                <Button onClick={handleLogout}
+                        className="btn-primary"
+                        label="LOG OUT"
+                        type="submit"  />
+            </div>
 
+        </div>
+    )
+}
 export default Trainer;
